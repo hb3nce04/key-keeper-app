8a6cc01c8b840064001b7e450a7b6b0c04acd15e
hb3nce04 <60313402+hb3nce04@users.noreply.github.com>
2025-09-10 20:16:16 +0200

    feat(backend): initialize Spring project

---
+++ backend/src/main/java/io/hb3nce04/keykeeperapp/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package io.hb3nce04.keykeeperapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return httpSecurity.build();
+    }
+}
