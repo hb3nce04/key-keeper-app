a097bd595474a0e19a0fb778321dcdb610e3108d
hb3nce04 <60313402+hb3nce04@users.noreply.github.com>
2025-09-10 19:21:38 +0200

    Initial commit

---
+++ backend/src/main/java/io/hb3nce04/keykeeperapp/KeyKeeperAppApplication.java
@@ -0,0 +1,13 @@
+package io.hb3nce04.keykeeperapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyKeeperAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyKeeperAppApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/io/hb3nce04/keykeeperapp/KeyKeeperAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hb3nce04.keykeeperapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyKeeperAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
